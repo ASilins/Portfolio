b91e5f3ca512a3299ce91d1d0635f657f1e9ccea
Arturs Silins <80292989+ASilins@users.noreply.github.com>
2023-02-13 13:48:11 +0100

    server router

--- server/app.js
+++ server/app.js
@@ -1,8 +1,8 @@
-const express = require('express');
-const app = express();
+const express = require("express");
+import controller from "./controller";
 
-app.get("/api", (req, res) => {
-    res.json({ "users": ["userOne", "userTwo"] })
-})
+
+const app = express();
+app.use("/", controller);
 
 app.listen(5000, () => { console.log("Server started on port 5000") })
