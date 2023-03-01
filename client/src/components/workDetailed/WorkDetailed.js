3156c8d57289cc68a714c130af5685661cc26ef3
Arturs Silins <80292989+ASilins@users.noreply.github.com>
2023-03-01 20:45:43 +0100

    Information change

--- client/src/components/workDetailed/WorkDetailed.js
+++ client/src/components/workDetailed/WorkDetailed.js
@@ -6,14 +6,20 @@ function WorkDetailed(props) {
 
     const width = useScreenSize();
     let img = require("../../assets/default.jpg");
-    let imgSize = 35;
+    let imgSize = 45;
 
     if (width >= 480 && width < 840) {
-        imgSize = 40;
+        imgSize = 55;
     }
     else if (width >= 840) {
-        imgSize = 45;
+        imgSize = 60;
     }
+
+    const data = [
+        { id: 1, name: 'Row 1', column2: 'Row 1, Column 2' },
+        { id: 2, name2: 'Row 2', column2: null },
+        { id: 3, name3: 'Row 3', column2: 'Row 3, Column 2' },
+    ];
 
     // if (props.data.img === null) {
     //     img = require("../../assets/default.jpg");
@@ -36,8 +42,18 @@ function WorkDetailed(props) {
                     <p><a href="">Link to page</a></p>
 
                 </div>
+
                 <div className="workImageOne">
-                    <Image src={img} alt="Project photo" height={`${imgSize}rem`} speed={0.4} />
+                    <Image src={img} alt="Project photo" height={`${imgSize}rem`} speed={0.2} />
+                </div>
+
+                <div className="container workDetails">
+                    <table>
+                        <tr>
+                            <td>Client</td>
+                            <td>Lorem Ipsum</td>
+                        </tr>
+                    </table>
                 </div>
 
             </div>
